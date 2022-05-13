614af73d24403c9c98fee813c0f347f0792d6266
Thien Nguyen <kimthien175@gmail.com>
2022-05-13 12:49:55 +0700

    remove all cli content in root

--- lib/utils/dialog.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-void showCustomDialog(BuildContext context, String title, String content) {
-  showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(title: Text(title), content: Text(content));
-      });
-}
